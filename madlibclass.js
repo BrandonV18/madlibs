c4ff18e421a8c8497cd9078fd5c0d18d4692f340
BrandonV18 <31927431+BrandonV18@users.noreply.github.com>
2017-10-11 09:12:53 -0400

    Update madlibclass.js

--- madlibclass.js
+++ madlibclass.js
@@ -1,18 +1,30 @@
 // Enter Sentence
 var sentence = [];
 var replacements = [];
-var index = -1; //allow for incrementing in while loop
+var index = 0; //allow for incrementing in while loop
+
 filler(sentence,"Next Word:");
 filler(replacements, "Next Position:");
+replace(sentence,replacements,"Part of speech for word in sentence")
+alert("Switch positions");
    
    //Enter Replacements
 function filler(myArray,myPrompt){
    index = -1;
  do {
 	index++;
-   	myArray[index] = prompt(myPrompt +index+ ":");
+   	myArray[index] = prompt(myPrompt+"["+index+"] = "); //Where you enter number of array
   }
  while (myArray[index]!="^");
+ myArray.pop(); // takes ^ away at the end
 console.log(myArray); //debugging
  }
+ 
+ function replace(mySArray, myRArray, myPrompt){ // replacing words
+    for (index = 0;index < myRArray.length; index++){ // loop for words to replace 
+		mySArray[myRArray[index]] = prompt(myPrompt+"["+[myRArray[index]]+"] = "); // defining words to replace
+		}
+   console.log(mySArray);
+ }
+
  
